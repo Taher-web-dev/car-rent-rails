bc86952dae3ecae5d38fc41e3c9d5426c685bb00
Taher <ttaherhagui@gmail.com>
2022-03-30 10:06:49 +0100

    add cars.

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,5 +5,5 @@ class Car < ApplicationRecord
   validates :model, presence: true, length: { maximum: 20 }
   validates :model_year, presence: true, numericality: { greater_than: 1900 }, length: { maximum: 4 }
   validates :photo_url, presence: true
-  validates :description, presence: true, length: { maximum: 100 }
+  validates :description, presence: true
 end
