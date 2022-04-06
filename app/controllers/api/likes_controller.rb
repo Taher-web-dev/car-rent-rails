acb204d89b9f4d14011a0eda73dad0b8cd3ab68f
Taher <ttaherhagui@gmail.com>
2022-04-06 09:40:35 +0100

    add documentation for like model.

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,8 +1,24 @@
 class Api::LikesController < ApplicationController
+  def_param_group :like do
+    param :id, :number, desc: 'likes id'
+    param :like, Hash do
+      param :car_id, String, required: true, desc:'Id of car to be liked'
+      param :user_id, String, required: true, desc:'Id of the user who adds likes'
+    end
+    property :car_id, String, desc:'Id of car to be liked'
+    property :user_id, String, desc:'Id of the user who adds likes'
+    property :created_at, String, desc: 'Date of the like creation'
+    property :updated_at, String, desc: 'Last time the like was updated'
+
+  end
+  api :GET, '/likes/', 'Shows all likes'
   def index
     @likes = Like.all
     render json: { likes: @likes }
   end
+  api :POST, '/likes/', 'Manage a new like'
+  returns :like, code: 200, desc: 'Manage like'  
+  param_group :like
   def create
     @like = Like.find_by(likes_param)
     @car = Car.find(likes_param[:car_id])
