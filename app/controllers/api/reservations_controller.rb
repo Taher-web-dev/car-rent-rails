500e846a1d6a7fa490e0c01b4fc99948858eb1a4
Taher <ttaherhagui@gmail.com>
2022-04-06 11:00:11 +0100

    add reservation documentation.

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -2,11 +2,29 @@
 
 module Api
   class ReservationsController < ApplicationController
+    def_param_group :reservation do
+      param :id, :number, desc: 'reservations id'
+      param :reservation, Hash do
+        param :start_date, String, required: true, desc:'Start date of reservation'
+        param :city, String, required: true, desc:'City of client reservation'
+        param :user_id, String, required: true, desc:'The id of the client'
+        param :car_id, String, required: true, desc:'The id of the reserved car'
+      end
+      property :car_id, String, desc:'Id of car to be reserved'
+      property :user_id, String, desc:'Id of the client who asks for reservation'
+      property :start_date, String, desc:'Start date of reservation reservation'
+      property :city, String, desc:'the city of the car reservation'
+      property :created_at, String, desc: 'Date of the reservation creation'
+      property :updated_at, String, desc: 'Last time the reservation was updated'
+    end
+    api :GET, '/reservations', 'Show all reservations'
     def index
       @reserves = Reservation.all
       render json: { reserves: @reserves }.to_json
     end
-
+    api :POST, '/reservations/', 'Add a new reservation'
+    returns :reservation, code: 200, desc: 'Manage like'  
+    param_group :reservation
     def create
       reserve = Reservation.new(reserve_param)
       if reserve.save
@@ -29,7 +47,8 @@ module Api
       end
     end
 
-
+    api :DELETE, '/reservations/:id', 'Delete a specific reservation'
+    returns code: 200, desc: 'Reservation successfully deleted.'
     def destroy
       @reservation = Reservation.find(params[:id])
       if @reservation.destroy
