ab1392f6c73e3d9eb1e4d09d59c6b666ce5c4b15
Taher <ttaherhagui@gmail.com>
2022-04-07 16:31:05 +0100

    fix like controlelr.

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,25 +1,11 @@
 module Api
 class LikesController < ApplicationController
-  def_param_group :likes do
-    param :id, :number, desc: 'likes id'
-    param :likes, Hash do
-      param :car_id, String, required: true, desc:'Id of car to be liked'
-      param :user_id, String, required: true, desc:'Id of the user who adds likes'
-    end
-    property :car_id, String, desc:'Id of car to be liked'
-    property :user_id, String, desc:'Id of the user who adds likes'
-    property :created_at, String, desc: 'Date of the like creation'
-    property :updated_at, String, desc: 'Last time the like was updated'
-
-  end
-  api :GET, '/likes/', 'Shows all likes'
+  
   def index
     @likes = Like.all
     render json: { likes: @likes }
   end
-  api :POST, '/likes/', 'Manage a new like'
-  returns :likes, code: 200, desc: 'Manage like' 
-  param_group :likes
+ 
   def create
     @like = Like.find_by(likes_param)
     @car = Car.find(likes_param[:car_id])
@@ -43,5 +29,4 @@ class LikesController < ApplicationController
   def likes_param
     params.require(:likes).permit(:user_id, :car_id)
   end
-end
 end
