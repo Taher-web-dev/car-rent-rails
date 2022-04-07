e6b3ed7a753bfe7af709b0941643760f66e80024
Taher <ttaherhagui@gmail.com>
2022-04-07 12:29:15 +0100

    delete end like controller.

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -24,24 +24,22 @@ class Api::LikesController < ApplicationController
     @car = Car.find(likes_param[:car_id])
     like_nbr = @car.likes_counter
     if @like
-        @car.update(likes_counter: like_nbr - 1 )
-        @like.destroy
-        respond_to do |format|
-          format.json { render json: 'Likes decremented successfully!'.to_json, status: :ok }
-        end
-      else
-        @car.update(likes_counter: like_nbr + 1)
-        Like.create(likes_param)
-        respond_to do |format|
-          format.json { render json: 'Likes incremented successfully!'.to_json, status: :ok }
-        end
+      @car.update(likes_counter: like_nbr - 1 )
+      @like.destroy
+      respond_to do |format|
+        format.json { render json: 'Likes decremented successfully!'.to_json, status: :ok }
+      end
+    else
+      @car.update(likes_counter: like_nbr + 1)
+      Like.create(likes_param)
+      respond_to do |format|
+        format.json { render json: 'Likes incremented successfully!'.to_json, status: :ok }
       end
     end
+  end
 
-    private
-
-    def likes_param
-      params.require(:likes).permit(:user_id, :car_id)
-    end
+  private
+  def likes_param
+    params.require(:likes).permit(:user_id, :car_id)
   end
 end
