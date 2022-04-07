6d98d8e9937b2cee94a9909e74618e758b47955a
Taher <ttaherhagui@gmail.com>
2022-04-07 16:50:59 +0100

    add route for create user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@
 
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
+  validates :photo, presence: false
+  has_many :likes, dependent: :destroy
+  has_many :reservations, dependent: :destroy
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
-    resources :users, only: [:index]
+    resources :users, only: [:index, :create]
     resources :cars, only: [:index, :create, :destroy, :update]
     resources :reservations, only: [:index, :create, :destroy, :update]
     resources :current_infos, only: [:index]
